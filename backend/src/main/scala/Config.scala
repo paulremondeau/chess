68c64c2cbe8c59afc419a5cdf089d182569a64b9
paulremondeau <poweraider91@gmail.com>
2023-08-13 18:38:18 +0200

    update config

--- backend/src/main/scala/Config.scala
+++ backend/src/main/scala/Config.scala
@@ -1,2 +1,4 @@
 final val frontEndUrl: String = sys.env.get("FRONTEND_URL").get
 final val frontEndPort: Int = sys.env.get("FRONTEND_PORT").get.toInt
+// final val frontEndUrl: String = "localhost"
+// final val frontEndPort: Int = 5173
--- frontend/config.ts
+++ frontend/config.ts
@@ -1,3 +1,5 @@
+// const backendUrl: String = "http://localhost:8000/"
 const backendUrl: String = "https://chess-backend-27lngjqnaq-od.a.run.app/"
+
 
 export default backendUrl
